8e8c056add5ae6e05ae169708ae8421d90cde99f
Sofiia Samigulina <sonya200397@gmail.com>
2025-08-12 21:57:11 +0200

    Also little transformations to English final-script.sql

--- final-script.sql
+++ final-script.sql
@@ -1,6 +1,6 @@
 --sequences for triggers
-DROP SEQUENCE IF EXISTS empl_id_od100;
-CREATE SEQUENCE empl_id_od100
+DROP SEQUENCE IF EXISTS dep_id_od100;
+CREATE SEQUENCE dep_id_od100
 INCREMENT 1
 START 100;
 
@@ -102,145 +102,145 @@ CREATE TABLE payments (
 );
 
 --triggers autoincrements
-CREATE OR REPLACE FUNCTION set_pracov_incr()
+CREATE OR REPLACE FUNCTION set_dep_incr()
     RETURNS TRIGGER
     AS $$
         BEGIN
-            IF NEW.id_pracoviska IS NULL then
-                NEW.id_pracoviska := nextval('prac_id_od100');
+            IF NEW.department_id IS NULL then
+                NEW.department_id := nextval('dep_id_od100');
             end if;
         RETURN NEW;
         END;
     $$LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION set_cislo_obj_tura()
+CREATE OR REPLACE FUNCTION set_order_num_tours()
     RETURNS TRIGGER
     AS $$
         BEGIN
-            IF NEW.cislo_objednavky IS NULL then
-                NEW.cislo_objednavky := nextval('cislo_obj_tura');
+            IF NEW.order_number IS NULL then
+                NEW.order_number := nextval('order_num_tours');
             end if;
         RETURN NEW;
         END;
     $$LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION set_cislo_obj_platba()
+CREATE OR REPLACE FUNCTION set_order_num_payments()
     RETURNS TRIGGER
     AS $$
         BEGIN
-            IF NEW.cislo_objednavky IS NULL then
-                NEW.cislo_objednavky := nextval('cislo_obj_platba');
+            IF NEW.order_number IS NULL then
+                NEW.order_number := nextval('order_num_payments');
             end if;
         RETURN NEW;
         END;
     $$LANGUAGE plpgsql;
 
-CREATE TRIGGER trigger_set_pracov_id
-BEFORE INSERT ON oddelenie
+CREATE TRIGGER trigger_set_dep_id
+BEFORE INSERT ON departments
 FOR EACH ROW
-EXECUTE FUNCTION set_pracov_incr();
+EXECUTE FUNCTION set_dep_incr();
 
-CREATE TRIGGER trigger_set_obj_tura
-BEFORE INSERT ON tura
+CREATE TRIGGER trigger_set_order_num_tours
+BEFORE INSERT ON tours
 FOR EACH ROW
-EXECUTE FUNCTION set_cislo_obj_tura();
+EXECUTE FUNCTION set_order_num_tours();
 
-CREATE TRIGGER trigger_set_obj_platba
-BEFORE INSERT ON platba
+CREATE TRIGGER trigger_set_order_num_paym
+BEFORE INSERT ON payments
 FOR EACH ROW
-EXECUTE FUNCTION set_cislo_obj_platba();
+EXECUTE FUNCTION set_order_num_payments();
 
 --inserts
-INSERT INTO zakaznik
-    (meno, priezvisko, telefon, email, datum_narodenia, datum_prveho_nakupu, poistenie, kde_sa_dozvedel_onas, cislo_dokladu, obcianstvo)
+INSERT INTO clients
+    (name, surname, phone, email, birthday_date, date_first_purchase, insurance, how_heard_about_us, document_number, citizenship)
 VALUES
-    ('Alexandra', 'Polakova', 0987236451, 'sandra.polakova@yahoo.com', '1976-10-10', '2024-12-05', 'VZP', NULL, '761010/8517', 'Slovensko'),
-    ('Sonya', 'Migasova', 0959163544, 'sonya200397@gmail.com', '1997-03-20', '2024-12-10', NULL, 'Instagram', '975320/8756', 'Rusko'),
-    ('Branislav', 'Kucera', 0968962123, 'kucera88.branislav@gmail.com', '1988-05-10', '2024-12-15', NULL, 'kamarati', 'P23556911', 'Slovensko'),
-    ('Ivana', 'Horvathova', 0948659727, 'ivankaa1998@gmail.com', '1998-02-20', '2024-12-20', 'UNION', 'kamarati', '985220/6056', 'Slovensko'),
-    ('Mirka', 'Novakova', 0998242289, 'novakova_m@outlook.com', '1989-11-29', '2025-01-05', 'VZP', 'Instagram', '896129/2439', 'Slovensko'),
-    ('Branislav', 'Tomcikov', 0967891233, 'branislav10021978@outlook.com', '1978-02-10', '2025-01-07', NULL, 'Facebook', 'P45612978', 'Slovensko'),
-    ('Alina', 'Vasilenko', 0967995131, 'vasilenko_a@gmail.com', '1994-05-15', '2025-01-10', NULL, 'Facebook', '945515/8779', 'Ukrajina'),
-    ('Samuel', 'Novacikov', 0977555111, 'novacikov_1968@yahoo.com', '1968-04-13', '2025-01-17', 'UNION', NULL, '680413/3787', 'Slovensko'),
-    ('Juraj', 'Molnar', 0931998462, 'molnar_juraj@outlook.com', '1974-12-03', '2025-01-23', 'Dovera', 'Facebook', '741203/1506', 'Slovensko'),
-    ('Julia', 'Ivanova', 0936778123, 'ivanolia@gmail.com', '1988-01-31', '2025-02-01', NULL, 'Instagram', '885131/1743', 'Ukrajina');
+    ('Alexandra', 'Polakova', 0987236451, 'sandra.polakova@yahoo.com', '1976-10-10', '2024-12-05', 'VZP', NULL, '761010/8517', 'Slovakia'),
+    ('Sonya', 'Migasova', 0959163544, 'sonya200397@gmail.com', '1997-03-20', '2024-12-10', NULL, 'Instagram', '975320/8756', 'Russia'),
+    ('Branislav', 'Kucera', 0968962123, 'kucera88.branislav@gmail.com', '1988-05-10', '2024-12-15', NULL, 'friends', 'P23556911', 'Slovakia'),
+    ('Ivana', 'Horvathova', 0948659727, 'ivankaa1998@gmail.com', '1998-02-20', '2024-12-20', 'UNION', 'friends', '985220/6056', 'Slovakia'),
+    ('Mirka', 'Novakova', 0998242289, 'novakova_m@outlook.com', '1989-11-29', '2025-01-05', 'VZP', 'Instagram', '896129/2439', 'Slovakia'),
+    ('Branislav', 'Tomcikov', 0967891233, 'branislav10021978@outlook.com', '1978-02-10', '2025-01-07', NULL, 'Facebook', 'P45612978', 'Slovakia'),
+    ('Alina', 'Vasilenko', 0967995131, 'vasilenko_a@gmail.com', '1994-05-15', '2025-01-10', NULL, 'Facebook', '945515/8779', 'Ukraine'),
+    ('Samuel', 'Novacikov', 0977555111, 'novacikov_1968@yahoo.com', '1968-04-13', '2025-01-17', 'UNION', NULL, '680413/3787', 'Slovakia'),
+    ('Juraj', 'Molnar', 0931998462, 'molnar_juraj@outlook.com', '1974-12-03', '2025-01-23', 'Dovera', 'Facebook', '741203/1506', 'Slovakia'),
+    ('Julia', 'Ivanova', 0936778123, 'ivanolia@gmail.com', '1988-01-31', '2025-02-01', NULL, 'Instagram', '885131/1743', 'Ukraine');
 
-INSERT INTO oddelenie
-    (meno_riaditela, priezvisko_riaditela, telefon_riaditela, email_riaditela, kolko_zamestnancov, nazov_oddelenia)
+INSERT INTO departments
+    (name_dir, surname_dir, phone_dir, email_dir, how_many_employees, department_name)
 VALUES
-    ('Samuel', 'Migas', 0956725463, 'migas_it@outlook.com', 3, 'Technicke oddelenie'),
-    ('Antonia', 'Vargova', 0900555666, 'vargova_predaj@outlook.com', 7, 'Predajne oddelenie'),
-    ('Greta', 'Blanarova', 0983421655, 'blanarova_pr@outlook.com', 3, 'Reklamne oddelenie'),
-    ('Blazej','Vinogradov', 0924569781, 'vinogradov_support@outlook.com', 2, 'Zakaznicke oddelenie'),
-    ('Irina','Tereshchuk', 0956698123, 'tereshchuk_fin@outlook.com', 2, 'Financne oddelenie');
+    ('Samuel', 'Migas', 0956725463, 'migas_it@outlook.com', 3, 'Technical Department'),
+    ('Antonia', 'Vargova', 0900555666, 'vargova_predaj@outlook.com', 7, 'Sales Department'),
+    ('Greta', 'Blanarova', 0983421655, 'blanarova_pr@outlook.com', 3, 'Marketing Department'),
+    ('Blazej','Vinogradov', 0924569781, 'vinogradov_support@outlook.com', 2, 'Customer Service Department'),
+    ('Irina','Tereshchuk', 0956698123, 'tereshchuk_fin@outlook.com', 2, 'Finance Department');
 
-INSERT INTO zamestnanec
-    (meno, priezvisko, pozicia, telefon, email, ID_zamestnanca, ID_pracoviska, vzdelanie, kedy_nastupil_doprace, mesacny_plat, ma_deti)
+INSERT INTO employees
+    (name, surname, position, phone, email, employe_id, department_id, degree, date_employment, salary, children)
 VALUES
-    ('Katka', 'Novakova', 'predajca zajazdov', 0904123890, 'katka.novakova@outlook.com', 1011, 101, NULL, '2024-11-25', 1350, 'nie'),
-    ('Pavol', 'Petrov', 'specialista mestskej reklamy', 0919456789, 'pavol.petrov@outlook.com', 1021, 102, NULL, '2024-11-25', 1350, 'ano'),
-    ('Mirka', 'Vargova', 'predajca zajazdov', 0905678901, 'mirka.vargova@outlook.com', 1012, 101, NULL, '2024-12-05', 1350, 'ano'),
-    ('Norbert', 'Varga', 'referent pre reklamacie', 0912456789, 'norbert.varga@outlook.com', 1015, 101, NULL, '2024-12-15', 1050, NULL),
-    ('Diana', 'Kovalenko', 'specialista call centru', 0907345678, 'diana.kovalenko@outlook.com', 1031, 103, NULL, '2024-12-15', 1000, 'nie'),
-    ('Alexandra', 'Polarikova', 'referent pre reklamacie', 0915678901, 'alexandra.polarikova@outlook.com', 1016, 101, 'Obchodna sprava', '2024-12-17', 1050, 'nie'),
-    ('Samuel','Novakov', 'uctovnik', 0919654321, 'samuel.novakov@outlook.com', 1041, 104, NULL, '2024-12-18', 1050, 'ano'),
-    ('Norbert', 'Kovac', 'predajca zajazdov', 0910234567, 'norbert.kovac@outlook.com', 1013, 101, 'Obchodna sprava', '2025-01-03', 1450, 'ano'),
-    ('Robert', 'Polak', 'predajca zajazdov', 0911987654, 'robert.polak@outlook.com', 1014, 101, 'Obchodna sprava', '2025-01-03', 1400, 'nie'),
-    ('Samuel', 'Bechera', 'frontend developer', 0902987654, 'samuel.bechera@outlook,com', 1001, 100, 'Informatika', '2025-01-10', 2500, NULL),
-    ('Michal', 'Vadimov', 'online supporter', 0948234567, 'michal.vadimov@outlook.com', 1032, 103, 'Veterinaria', '2025-01-16', 1200, NULL),
-    ('Richard', 'Polarik', 'specialista telefonnej reklamy', 0940678901, 'richard.polarik@outlook.com',1022, 102, NULL, '2025-01-17', 1350, 'nie'),
-    ('Dusan', 'Ambros', 'QR inzinier', 0903456789, 'dusan.ambros@outlook.com', 1002, 100, NULL, '2024-01-19', 1850, 'ano'),
-    ('Anya', 'Kirsanova', 'social media manazer', 0918987654, 'anya.kirsanova@outlook.com', 1023, 101, 'Reklama a marketing', '2025-01-21', 1300, NULL),
-    ('Kristina', 'Odincova','analytik predaja', 0917234567, 'kristina.odincova@outlook.com', 1017, 101, 'Analytik dat', '2025-01-25', 1500, 'ano'),
-    ('Ivana', 'Kovacova', 'uctovnik', 0902789012, 'ivana.kovacova@outlook.com', 1042, 104, 'Financna sprava', '2025-02-05', 1050, NULL),
-    ('Sofiia', 'Samigulina', 'databazovy spravca', 0901234567, 'sofiia.samigulina@outlook.com', 1003, 100, 'Informatika', '2025-02-05',  1600, NULL);
+    ('Katka', 'Novakova', 'Tour Sales Agent', 0904123890, 'katka.novakova@outlook.com', 1011, 101, NULL, '2024-11-25', 1350, 'no'),
+    ('Pavol', 'Petrov', 'Outdoor Advertising Specialist', 0919456789, 'pavol.petrov@outlook.com', 1021, 102, NULL, '2024-11-25', 1350, 'yes'),
+    ('Mirka', 'Vargova', 'Tour Sales Agent', 0905678901, 'mirka.vargova@outlook.com', 1012, 101, NULL, '2024-12-05', 1350, 'yes'),
+    ('Norbert', 'Varga', 'Complaints Officer', 0912456789, 'norbert.varga@outlook.com', 1015, 101, NULL, '2024-12-15', 1050, NULL),
+    ('Diana', 'Kovalenko', 'Call Center Specialist', 0907345678, 'diana.kovalenko@outlook.com', 1031, 103, NULL, '2024-12-15', 1000, 'no'),
+    ('Alexandra', 'Polarikova', 'Complaints Officer', 0915678901, 'alexandra.polarikova@outlook.com', 1016, 101, 'Sales Management', '2024-12-17', 1050, 'no'),
+    ('Samuel','Novakov', 'Accountant', 0919654321, 'samuel.novakov@outlook.com', 1041, 104, NULL, '2024-12-18', 1050, 'yes'),
+    ('Norbert', 'Kovac', 'Tour Sales Agent', 0910234567, 'norbert.kovac@outlook.com', 1013, 101, 'Sales Management', '2025-01-03', 1450, 'yes'),
+    ('Robert', 'Polak', 'Tour Sales Agent', 0911987654, 'robert.polak@outlook.com', 1014, 101, 'Sales Management', '2025-01-03', 1400, 'no'),
+    ('Samuel', 'Bechera', 'frontend developer', 0902987654, 'samuel.bechera@outlook,com', 1001, 100, 'Informatics', '2025-01-10', 2500, NULL),
+    ('Michal', 'Vadimov', 'online supporter', 0948234567, 'michal.vadimov@outlook.com', 1032, 103, 'Veterinary Science', '2025-01-16', 1200, NULL),
+    ('Richard', 'Polarik', 'Telemarketing Specialist', 0940678901, 'richard.polarik@outlook.com',1022, 102, NULL, '2025-01-17', 1350, 'no'),
+    ('Dusan', 'Ambros', 'QR engineer', 0903456789, 'dusan.ambros@outlook.com', 1002, 100, NULL, '2024-01-19', 1850, 'yes'),
+    ('Anya', 'Kirsanova', 'social media manager', 0918987654, 'anya.kirsanova@outlook.com', 1023, 101, 'Reklama a marketing', '2025-01-21', 1300, NULL),
+    ('Kristina', 'Odincova','Sales Analyst', 0917234567, 'kristina.odincova@outlook.com', 1017, 101, 'Data Analyst', '2025-01-25', 1500, 'yes'),
+    ('Ivana', 'Kovacova', 'Accountant', 0902789012, 'ivana.kovacova@outlook.com', 1042, 104, 'Finance Management', '2025-02-05', 1050, NULL),
+    ('Sofiia', 'Samigulina', 'Database Administrator', 0901234567, 'sofiia.samigulina@outlook.com', 1003, 100, 'Informatics', '2025-02-05',  1600, NULL);
 
-INSERT INTO tura
-    (datum_odchoda, typ_dopravy, kde_je_start, kam, typ_cestovania, typ_ubytovania, kolko_dni, cislo_dokladu_zakaznika, ID_zamestnanca)
+INSERT INTO tours
+    (departure_day, transport_type, starting_location, journey_to, travelling_type, accomodation_type, how_many_days, client_doc_number, employe_id)
 VALUES
-    ('2025-02-04', 'lietadlo', 'Vieden', 'Spanielsko', 'turizmus', 'hostel', 5, '761010/8517', 1011),
-    ('2025-03-08', 'lietadlo', 'Budapest', 'Francuzko', 'turizmus', 'hotel', 3, '975320/8756',1012),
-    ('2025-03-10', 'lietadlo', 'Vieden', 'Amerika', NULL, 'hotel', 3, 'P23556911',1013),
-    ('2025-01-15', 'lietadlo', 'Budapest', 'Slovinsko', 'navsteva', NULL, 5, '985220/6056', 1014),
-    ('2025-02-12', 'vlak', 'Kosice', 'Vieden', 'business', 'hotel', 4, '896129/2439', 1011),
-    ('2025-04-07', 'lietadlo', 'Budapest', 'Rusko', 'turizmus', 'byt', 7, 'P45612978', 1012),
-    ('2025-03-23', 'vlak', 'Kosice', 'Svajciarsko', 'put', NULL, 3,'945515/8779', 1013),
-    ('2025-03-24', 'autobus', 'Kosice', 'Varsava', 'turizmus', 'byt', 6, '680413/3787', 1014),
-    ('2025-04-01','lietadlo', 'Budapest', 'Taliansko', 'turizmus', 'hotel', 10, '741203/1506', 1011),
-    ('2025-05-05', 'lietadlo', 'Vieden', 'Spanielsko', 'turizmus', 'byt', 11, '885131/1743', 1012);
+    ('2025-02-04', 'airplane', 'Vienna', 'Spain', 'tourism', 'hostel', 5, '761010/8517', 1011),
+    ('2025-03-08', 'airplane', 'Budapest', 'France', 'tourism', 'hotel', 3, '975320/8756',1012),
+    ('2025-03-10', 'airplane', 'Vienna', 'USA', NULL, 'hotel', 3, 'P23556911',1013),
+    ('2025-01-15', 'airplne', 'Budapest', 'Slovenia', 'visiting', NULL, 5, '985220/6056', 1014),
+    ('2025-02-12', 'train', 'Kosice', 'Vienna', 'business', 'hotel', 4, '896129/2439', 1011),
+    ('2025-04-07', 'airplane', 'Budapest', 'Russia', 'tourism', 'flat', 7, 'P45612978', 1012),
+    ('2025-03-23', 'train', 'Kosice', 'Switzerland', 'Pilgrimage', NULL, 3,'945515/8779', 1013),
+    ('2025-03-24', 'bus', 'Kosice', 'Warsaw', 'tourism', 'flat', 6, '680413/3787', 1014),
+    ('2025-04-01','airplane', 'Budapest', 'Italy', 'tourism', 'hotel', 10, '741203/1506', 1011),
+    ('2025-05-05', 'airplane', 'Vienna', 'Spain', 'tourism', 'flat', 11, '885131/1743', 1012);
 
-INSERT INTO platba
-    (cislo_platby, datum_platby, typ_platby, sum, bonusy_akcie, cislo_darcekoveho_preukazu, cislo_karty_IBAN)
+INSERT INTO payments
+    (payment_id, payment_date, payment_type, total, special_offers, gift_card_num, iban)
 VALUES
-    (19872, '2024-12-05', 'bankovska karta, darcekovy preukaz', 155.28,  NULL, 202400, 'SK78 1122 3344 5566 7788 9900'),
-    (47932, '2024-12-10', 'hotovost', 348,  'vianocne -15%', NULL, NULL),
-    (6689, '2024-12-15', 'hotovost, darcekovy preukaz', 1335.50, 'vianocne -15%', 202401, NULL),
-    (4586, '2024-12-20', 'bankovska karta', 479.95,  'vianocne -15%', NULL, 'SK64 0054 0041 7432 1275 4217'),
-    (65487, '2025-01-05', 'darcekovy preukaz', 150,   NULL, 202402, NULL),
-    (4239, '2025-01-07', 'bankovska karta', 1500.75,  '-30% na 1.cestu mimo Shengenu', NULL, 'SK45 9876 5432 1098 7654 3210'),
-    (5697, '2025-01-10', 'bankovska karta', 750.50, NULL, NULL,  'SK12 1234 5678 9012 3456 7890'),
-    (3246, '2025-01-17', 'bankovska karta', 342.55,   NULL, NULL, 'SK68 4210 8700 1714 3751 7491'),
-    (1256, '2025-01-23', 'bankovska karta', 850.75,   NULL, NULL, 'SK68 7500 0000 0012 3456 7892'),
-    (21685, '2025-02-01', 'hotovost', 500,  '-10% na narodeniny', NULL, NUll);
+    (19872, '2024-12-05', 'debit card, gift card', 155.28,  NULL, 202400, 'SK78 1122 3344 5566 7788 9900'),
+    (47932, '2024-12-10', 'cash', 348,  'Christmas sale -15%', NULL, NULL),
+    (6689, '2024-12-15', 'cash, gift card', 1335.50, 'Christmas sale -15%', 202401, NULL),
+    (4586, '2024-12-20', 'debit card', 479.95,  'Christmas sale -15%', NULL, 'SK64 0054 0041 7432 1275 4217'),
+    (65487, '2025-01-05', 'gift card', 150,   NULL, 202402, NULL),
+    (4239, '2025-01-07', 'debit card', 1500.75,  '30% off your first trip outside Schengen', NULL, 'SK45 9876 5432 1098 7654 3210'),
+    (5697, '2025-01-10', 'debit card', 750.50, NULL, NULL,  'SK12 1234 5678 9012 3456 7890'),
+    (3246, '2025-01-17', 'debit card', 342.55,   NULL, NULL, 'SK68 4210 8700 1714 3751 7491'),
+    (1256, '2025-01-23', 'debit card', 850.75,   NULL, NULL, 'SK68 7500 0000 0012 3456 7892'),
+    (21685, '2025-02-01', 'cash', 500,  '-10% birthday sale', NULL, NUll);
 
 --function
-CREATE OR REPLACE FUNCTION changeDokuments()
+CREATE OR REPLACE FUNCTION changeDocuments()
     RETURNS TRIGGER
     AS $$
         BEGIN
-            IF NEW.cislo_dokladu <> OLD.cislo_dokladu THEN
-                UPDATE tura
-                SET cislo_dokladu_zakaznika = NEW.cislo_dokladu
-                WHERE cislo_dokladu_zakaznika = OLD.cislo_dokladu;
+            IF NEW.document_number <> OLD.document_number THEN
+                UPDATE tours
+                SET client_doc_number = NEW.document_number
+                WHERE client_doc_numbet = OLD.document_number;
             end if;
             RETURN NEW;
         END;
     $$LANGUAGE plpgsql;
 
 CREATE TRIGGER trigger_updateDocs
-AFTER UPDATE OF cislo_dokladu ON zakaznik
+AFTER UPDATE OF document_number ON clients
 FOR EACH ROW
-EXECUTE FUNCTION changeDokuments();
+EXECUTE FUNCTION changeDocuments();
 
 --creating views
 
@@ -379,4 +379,5 @@ CREATE OR REPLACE FUNCTION riaditel_ma_zamestnancov ()
     $$ language plpgsql;
 
 select riaditel_ma_zamestnancov();
+
 
